8bb54b0545453b808294f034ab0259ff2701c2fe
wuhao <308801737@qq.com>
2017-10-09 16:21:08 +0800

    exemple app.js

---
+++ exemple/app.js
@@ -0,0 +1,49 @@
+
+
+const SyBase = require('./db')
+SyBase.use(__dirname + '/models')
+
+const CRUD = () => {
+  SyBase.models.WXUser
+    .by(SyBase.DBPools.main)
+    .create({
+      id: 12345678,
+      openid: 'test Openid',
+      phone: '11',
+      headurl: 'http://www.baidu.com/favicon',
+      cardno: '90000011',
+      wxname: 110
+    })
+    .then(res => {
+      console.info('------- Created ', res)
+      return SyBase.models.WXUser.findByID(res.id)
+    })
+    .then(res => {
+      console.info('------- Find BY ID : ', res)
+      return SyBase.models.WXUser.findOne({ id: res.id })
+    })
+    .then(res => {
+      console.info('------- Find One : ', res)
+      return SyBase.models.WXUser.findAll({ id: res.id })
+    })
+    .then(res => {
+      console.info('------- Find All : ', res)
+      return SyBase.models.WXUser.update({ wxname: 'updated name' }, { id: res[0].id })
+    })
+    .then(res => {
+      console.info('------- Updated :', res)
+      return SyBase.models.WXUser.delete({ id: res.id })
+    })
+    .then(CRUD)
+    .catch(err => {
+      console.error('********* Error :', err)
+      SyBase.models.WXUser.delete({ cardno: '90000011' })
+    })
+}
+
+CRUD()
+
+// SyBase.models.WXUser.by(SyBase.DBPools.main).execSql("select * from t_wxuser where id>=2000000 AND phone='15813880161'").then(res => console.info('-------- FindOne :', res))
+
+
+
